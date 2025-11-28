23f647c96dd602cffe0c3bd23d0e784581ce4534
AltinaCS <152580142+AltinaCS@users.noreply.github.com>
2025-11-28 21:48:43 +0800

    Service目前是Optional，如果需要再放即可

---
+++ src/main/java/org/example/concertlotterysystem/repository/Repository.java
@@ -0,0 +1,4 @@
+package org.example.concertlotterysystem.repository;
+
+public class Repository {
+}
---
+++ src/main/java/org/example/concertlotterysystem/services/naninaniService.java
@@ -0,0 +1,4 @@
+package org.example.concertlotterysystem.services;
+
+public class naninaniService {
+}
