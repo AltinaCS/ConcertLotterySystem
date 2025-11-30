aa43ad348ae839508cc6530d0c4bb543d3815f7e
boneDIO0 <stl930916@gmail.com>
2025-12-01 04:16:30 +0800

    實作 EventStatus enum

--- src/main/java/org/example/concertlotterysystem/entities/EventStatus.java
+++ src/main/java/org/example/concertlotterysystem/entities/EventStatus.java
@@ -1,7 +1,9 @@
 package org.example.concertlotterysystem.entities;
 
 public enum EventStatus {
-    OPEN,
-    CLOSE,
-    DRAWN
+    DRAFT,              // 已建活動但尚未開放報名
+    OPEN,  // 開放會員報名/抽籤登記
+    CLOSED,             // 截止報名，等待或準備開獎
+    DRAWN,              // 已開獎
+    CANCELLED           // 活動取消
 }
